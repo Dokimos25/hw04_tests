67b3597a20d0a020c32e2ddc4e52cef2ae204c68
Анна Королькова <koranta18@yandex.ru>
2023-03-22 16:29:49 +0400

    Version 4.10

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,7 +6,7 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['text', 'group',]
+        fields = ['text', 'group']
         labels = {
             'text': 'Введите текст',
             'group': 'Выберите группу'
